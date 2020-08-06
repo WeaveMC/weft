ea1887c0e8fe3f41adfdfc02c19ff80806d8f108
TheBrokenRail <connor24nolan@live.com>
2020-08-06 18:17:36 -0400

    Add World Events

---
+++ src/main/java/io/github/weavemc/weft/world/WorldEvents.java
@@ -0,0 +1,67 @@
+package io.github.weavemc.weft.world;
+
+/**
+ * https://github.com/FabricMC/yarn/pull/1652
+ */
+@SuppressWarnings("unused")
+public class WorldEvents {
+    public static final int DISPENSER_DISPENSE_ONE = 1000;
+    public static final int DISPENSER_FAIL = 1001;
+    public static final int DISPENSER_LAUNCH = 1002;
+    public static final int ENDER_EYE_LAUNCH = 1003;
+    public static final int FIREWORK_SHOOT = 1004;
+    public static final int IRON_DOOR_OPEN = 1005;
+    public static final int WOODEN_DOOR_OPEN = 1006;
+    public static final int WOODEN_TRAPDOOR_OPEN = 1007;
+    public static final int FENCE_GATE_OPEN = 1008;
+    public static final int FIRE_EXTINGUISH = 1009;
+    public static final int PLAY_MUSIC_DISC_DATA = 1010;
+    public static final int IRON_DOOR_CLOSE = 1011;
+    public static final int WOODEN_DOOR_CLOSE = 1012;
+    public static final int WOODEN_TRAPDOOR_CLOSE = 1013;
+    public static final int FENCE_GATE_CLOSE = 1014;
+    public static final int GHAST_WARN = 1015;
+    public static final int GHAST_SHOOT = 1016;
+    public static final int ENDER_DRAGON_SHOOT = 1017;
+    public static final int BLAZE_SHOOT = 1018;
+    public static final int ZOMBIE_ATTACK_WOODEN_DOOR = 1019;
+    public static final int ZOMBIE_ATTACK_IRON_DOOR = 1020;
+    public static final int ZOMBIE_BREAK_WOODEN_DOOR = 1021;
+    public static final int WITHER_BREAK_BLOCK = 1022;
+    public static final int WITHER_SHOOT = 1024;
+    public static final int BAT_TAKEOFF = 1025;
+    public static final int DISPENSER_DISPENSE_TWO = 1026;
+    public static final int ZOMBIE_INFECT = 1027;
+    public static final int ZOMBIE_VILLAGER_CONVERTED = 1028;
+    public static final int ANVIL_DESTROY = 1029;
+    public static final int ANVIL_USE = 1030;
+    public static final int ANVIL_LAND = 1031;
+    public static final int PORTAL_TRAVEL = 1032;
+    public static final int CHORUS_FLOWER_GROW = 1033;
+    public static final int CHORUS_FLOWER_DEATH = 1034;
+    public static final int BREWING_STAND_BREW = 1035;
+    public static final int IRON_TRAPDOOR_CLOSE = 1036;
+    public static final int IRON_TRAPDOOR_OPEN = 1037;
+    public static final int PHANTOM_BITE = 1039;
+    public static final int ZOMBIE_TO_DROWNED = 1040;
+    public static final int HUSK_TO_ZOMBIE = 1041;
+    public static final int GRINDSTONE_USE = 1042;
+    public static final int BOOK_PAGE_TURN = 1043;
+    public static final int SMITHING_TABLE_USE = 1044;
+    public static final int DRAGON_GROWL = 3001;
+    public static final int COMPOSTER_FILL_ACTIONS = 1500;
+    public static final int LAVA_EXTINGUISH_AND_SMOKE_PARTICLES = 1501;
+    public static final int REDSTONE_TORCH_BURNOUT_AND_SMOKE_PARTICLES = 1502;
+    public static final int END_PORTAL_FRAME_FILL_AND_SMOKE_PARTICLES = 1503;
+    public static final int BLOCK_BREAK_SOUND_AND_PARTICLES = 2001;
+    public static final int SPLASH_POTION_BREAK_ONE = 2002;
+    public static final int SPLASH_POTION_BREAK_TWO = 2007;
+    public static final int ENDER_EYE_FALL = 2003;
+    public static final int ENDER_EYE_BREAK = 2004;
+    public static final int DRAGON_FIREBALL_BREAK = 2006;
+    public static final int END_GATEWAY_SPAWN = 3000;
+    public static final int SMOKE_PARTICLES = 2000;
+    public static final int BONEMEAL_PARTICLES = 2005;
+    public static final int EXPLOSION = 2008;
+    public static final int CLOUD = 2009;
+}
